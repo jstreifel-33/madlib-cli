e4ed29b7becf4c1e4c366ecf0ae6d56952368d0c
Joe Streifel <josephstreifel@gmail.com>
2021-12-01 15:49:59 -0800

    completes merge function and adds FileNotFoundError. Preliminary tests passing

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,34 +1,36 @@
 import re
 
-PARTS_OF_SPEECH = 'Adjective|Noun|Verb|Adverb|Interjection'
+PARTS_OF_SPEECH = "Adjective|Noun|Verb|Adverb|Interjection"
 
-WELCOME_MESSAGE = '''Welcome to the Python Madlib!
+WELCOME_MESSAGE = """Welcome to the Python Madlib!
 This tool will ask you for some words,
 and then generate a funny text file
 to show your friends!
 
 Enter "start" to get started,
-or "help" for additional commands.'''
+or "help" for additional commands."""
 
-HELP_TEXT = '''Command options:
+HELP_TEXT = """Command options:
 start - begin generating a madlib
 view - display most recent madlib inside the console
 help - you are here
-exit - quit the program'''
+exit - quit the program"""
 
-
+class FileNotFoundError(Exception):
+    def __init__(self):
+        super().__init__()
 
 def read_template(file):
-    with open(file, 'r') as f:
+    with open(file, "r") as f:
         contents = f.read()
         return contents
 
 
-def parse_template(string):
-    words_required = re.findall(PARTS_OF_SPEECH, string)
-    stripped_template = re.sub(PARTS_OF_SPEECH, '', string)
+def parse_template(template):
+    words_required = re.findall(PARTS_OF_SPEECH, template)
+    stripped_template = re.sub(PARTS_OF_SPEECH, "", template)
     return stripped_template, tuple(words_required)
 
-def merge():
-    pass
 
+def merge(template, words):
+    return template.format(*words)
