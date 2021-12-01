fb9221293ea36dd692962945b68783ea81773aa6
Joe Streifel <josephstreifel@gmail.com>
2021-12-01 14:49:51 -0800

    creates welcome and help messages

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,3 +1,19 @@
+WELCOME_MESSAGE = '''Welcome to the Python Madlib!
+This tool will ask you for some words,
+and then generate a funny text file
+to show your friends!
+
+Enter "start" to get started,
+or "help" for additional commands.'''
+
+HELP_TEXT = '''Command options:
+start - begin generating a madlib
+view - display most recent madlib inside the console
+help - you are here
+exit - quit the program'''
+
+
+
 def read_template(file):
     with open(file, 'r') as f:
         contents = f.read()
