a04ba06e37f67d0cb8602eb21a3cc3caae6ff468
Joe Streifel <josephstreifel@gmail.com>
2021-12-01 21:30:58 -0800

    updates readme

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -29,33 +29,35 @@ Enter "help" to see a list of valid commands.
 
 ### USEFUL THINGS ###
 
+
 def border_print(message, top_bott_bord=True):
-  msg_list = message.splitlines()
-  msg_width = 0
+    msg_list = message.splitlines()
+    msg_width = 0
 
-  #Determine max width line in message
-  for line in msg_list:
-    if len(line) > msg_width:
-      msg_width = len(line)
+    # Determine max width line in message
+    for line in msg_list:
+        if len(line) > msg_width:
+            msg_width = len(line)
 
-  #Center lines based on pax width with 2 char whitepace padding
-  for idx, line in enumerate(msg_list):
-    msg_list[idx] = line.center(msg_width + 2)
+    # Center lines based on pax width with 2 char whitepace padding
+    for idx, line in enumerate(msg_list):
+        msg_list[idx] = line.center(msg_width + 2)
 
-  #Insert blank strings to list if top/bottom border not set False
-  if top_bott_bord:
-    msg_list.insert(0,'')
-    msg_list.insert(len(msg_list), '')
+    # Insert blank strings to list if top/bottom border not set False
+    if top_bott_bord:
+        msg_list.insert(0, "")
+        msg_list.insert(len(msg_list), "")
 
-  #Print each line of message, with added stars on side borders
-  for line in msg_list:
-    print(line.center(msg_width + 6, '*'))
-  
-  #White space
-  print('')
+    # Print each line of message, with added stars on side borders
+    for line in msg_list:
+        print(line.center(msg_width + 6, "*"))
+
+    # White space
+    print("")
 
 
 ### FILE HANDLING ###
+
 
 def read_template(file):
     try:
@@ -82,16 +84,17 @@ def merge(template, words):
 
 ### GAME ACTIONS ###
 
+
 def get_input(file="assets/dark_and_stormy_night_template.txt"):
-    command = input('> ')
+    command = input("> ")
     cmd_process(command, file)
     get_input(file)
-    
+
 
 def generate_madlib(file):
     raw_template = read_template(file)
     template, words = parse_template(raw_template)
-    
+
     user_words = []
 
     for word in words:
@@ -100,39 +103,42 @@ def generate_madlib(file):
 
     return merge(template, user_words)
 
+
 def save_madlib(content):
     with open("assets/madlib_result.txt", "w") as f:
         f.write(content)
     print('\nMadlib generated successfully!\nEnter "view" to display!\n')
 
+
 def select_madlib():
     pass
 
+
 def cmd_process(cmd, file):
-    if cmd == 'exit':
+    if cmd == "exit":
         print("\nGoodbye!")
         exit()
 
-    if cmd == 'help':
+    if cmd == "help":
         print(HELP_TEXT)
         return
 
-    if cmd == 'start':
+    if cmd == "start":
         madlib = generate_madlib(file)
         save_madlib(madlib)
         return
 
-    if cmd == 'view':
+    if cmd == "view":
         try:
             with open("assets/madlib_result.txt", "r") as f:
                 contents = f.read()
-                print(f'\nMost recent madlib:\n{contents}\n')
+                print(f"\nMost recent madlib:\n{contents}\n")
         except FileNotFoundError as e:
-            print('\nError: no existing madlib at path assets/madlibresults.txt\n')
+            print("\nError: no existing madlib at path assets/madlibresults.txt\n")
         return
 
-    if cmd == 'changelib':
-        print('\nComing Soon!\n')
+    if cmd == "changelib":
+        print("\nComing Soon!\n")
         return
 
     print(INVALID_TEXT)
@@ -140,6 +146,7 @@ def cmd_process(cmd, file):
 
 
 ### GAME RUNNING SCRIPT ###
+
 
 def start_game():
     border_print(WELCOME_MESSAGE)
